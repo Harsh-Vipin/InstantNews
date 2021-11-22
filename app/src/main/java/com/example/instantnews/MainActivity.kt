d0ebbfeb830113c272696aa7a9d3d65515c490bb
harshh-12 <harsh.vipinkp@gmail.com>
2021-11-22 16:25:54 +0530

    till data model

---
+++ app/src/main/java/com/example/instantnews/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.instantnews
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.example.instantnews.ui.theme.MyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyTheme {
+
+            }
+        }
+    }
+}
+
